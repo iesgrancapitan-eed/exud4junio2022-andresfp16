792ac4ef2e693ec3ae7b676d5103e0b34087a1b1
Andres <a21romaan@iesgrancapitan.org>
2022-06-09 08:52:26 +0200

    closes #1 refactor: encapsula atributos

--- src/com/examen/junio/Bicycle.java
+++ src/com/examen/junio/Bicycle.java
@@ -5,6 +5,19 @@ class Bicycle {
   int speed;
   int gear;
     
+  public int getSpeed() {
+    return speed;
+  }
+  public int getgear() {
+    return gear;
+  }
+  public void setspeed(int speed) {
+    this.speed = speed;
+  }
+  public void setsgear(int gear) {
+    this.gear = gear;
+  }
+  
   public void changeGear(int newGear){     
       gear = newGear;
   }
